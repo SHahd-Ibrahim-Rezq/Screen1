2962695875a081a8e04474ac2d580827ed044a8c
SHahd-Ibrahim-Rezq <sh777d000@gmail.com>
2025-03-12 17:07:58 +0200

    Updated README and main.dart, added test.dart

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Screen1
 CIS Task . Screen about fav food .
 =======
@@ -18,4 +17,3 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
->>>>>>> 14428c5 (Initial commit)
--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: ListView(              
-              children: [
-                Image.asset('lib/images/bashamel1.jpg'),
-                SizedBox(height: 20),
-                Text(
-                  'About',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'Egyptian macarona bechamel (مكرونة بشاميل,) is a comfort food recipe popular in Egypt made from penne pasta and a minced meat sauce baked with creamy béchamel. It features layers of penne pasta and a savory minced meat sauce, all enveloped in a creamy béchamel, then baked to golden perfection. A rich and satisfying dish perfect for family meals.',
-                  style: TextStyle(fontSize: 18),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'Instructions',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                SizedBox(height: 20),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Image.asset(
-                        'lib/images/step1.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Image.asset(
-                        'lib/images/step2.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Image.asset(
-                        'lib/images/step3.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Image.asset(
-                        'lib/images/step4.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-        
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        'lib/images/step5.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Image.asset(
-                        'lib/images/step6.png',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Image.asset(
-                        'lib/images/bashamel6.jpg',
-                        width: 100,
-                        height: 100,
-                      ),
-                      Image.asset(
-                        'lib/images/bashamel2.jpg',
-                        width: 100,
-                        height: 100,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SafeArea(child: Scaffold(body: HomeScreen(),)),);
+  }
+}
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ListView(
+        children: [
+          Image.asset('lib/images/bashamel1.jpg'),
+          _buildTitle('About'),
+          _buildDescribtion('Egyptian macarona bechamel (مكرونة بشاميل,) is a comfort food recipe popular in Egypt made from penne pasta and a minced meat sauce baked with creamy béchamel. It features layers of penne pasta and a savory minced meat sauce, all enveloped in a creamy béchamel, then baked to golden perfection. A rich and satisfying dish perfect for family meals.'),
+          _buildTitle('Instructions'),
+          _buildImageRow(['lib/images/step1.png',
+              'lib/images/step2.png',
+              'lib/images/step3.png',
+              'lib/images/step4.png',]),
+              _buildImageRow(['lib/images/step5.png',
+              'lib/images/step6.png',
+              'lib/images/bashamel6.jpg',
+              'lib/images/bashamel2.jpg'])
+        ]
       ),
     );
   }
+ 
+Widget _buildImageRow(List<String> imagePaths) {
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: List.generate(imagePaths.length, (index) => Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Image.asset(imagePaths[index], width: 90, height: 90),
+      )),
+    ),
+  );
+}
+Widget _buildTitle(String text){
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Text(text,style:TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
+  );
+}
+Widget _buildDescribtion(String text){
+  return Text(text,style: TextStyle(fontSize: 18),);
+}
 }
